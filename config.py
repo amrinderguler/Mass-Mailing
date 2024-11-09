af1eff5fcf100d0a295a43dd7bf78c4ab4d86737
Amrinder Singh <122505646+amrinderguler@users.noreply.github.com>
2024-11-09 17:52:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 # Email configuration
 EMAIL_HOST = 'smtp.gmail.com'  # Replace with your SMTP server
 EMAIL_PORT = 587  # Common port for TLS
-EMAIL_USER = 'amrindersingh292004@gmail.com'  # Your email address
-EMAIL_PASSWORD = 'ssrc xwrh hgfr pfwo'  # Your email password
+EMAIL_USER = # Your email address
+EMAIL_PASSWORD = # Your email password
 EMAIL_SUBJECT = 'Seeking Job Opportunities'  # Subject of the email
-LINKEDIN_URL = 'https://www.linkedin.com/in/amrinderguler'  # Your LinkedIn profile URL
-RESUME_PATH = 'Assets\Amrinder_Singh-Resume.pdf'  # Path to your resume file
+LINKEDIN_URL = 'https://www.linkedin.com/in/username'  # Your LinkedIn profile URL
+RESUME_PATH = 'path/to/resume'  # Path to your resume file
 
 EMAIL_BODY = '''\
 Hello,
 
-I hope this message finds you well. My name is Amrinder, and I am currently seeking job opportunities as a Python Developer at Test.it. I am very interested in the work your company is doing and believe my skills and experiences align well with your team.
+I hope this message finds you well. My name is [Your Name], and I am currently seeking job opportunities as a [Position] at [Company Name]. I am very interested in the work your company is doing and believe my skills and experiences align well with your team.
 
 I have attached my resume for your review, and you can also view my LinkedIn profile here: {linkedin_url}. I would greatly appreciate any guidance or opportunities you might have available.
 
 Thank you for your time, and I look forward to hearing from you.
 
 Best regards,
-Amrinder Singh
+[Yor Name]
 '''  # Sample email body
 
 # Path to the Excel file containing email addresses
-EXCEL_FILE_PATH = 'Assets\Book1.xlsx'  # Update with your actual file path
+EXCEL_FILE_PATH = 'path/to/your/excel-file'  # Update with your actual file path
